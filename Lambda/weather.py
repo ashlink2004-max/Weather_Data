344982c1a6c7c75c03318c8ce4e26f76a1e0a0fe
ashlink2004-max <ashlink2004@gmail.com>
2026-05-14 21:31:38 +0530

    Added event and record processing

--- Lambda/weather.py
+++ Lambda/weather.py
@@ -4,3 +4,14 @@ from datetime import datetime
 s3 = boto3.client('s3')
 
 BUCKET_NAME = 'weather-lamb-project1'
+
+print(event)
+
+    for record in event['Records']:
+
+        new_image = record['dynamodb'].get('NewImage', {})
+
+        data = {}
+
+        for key, value in new_image.items():
+            data[key] = list(value.values())[0]
