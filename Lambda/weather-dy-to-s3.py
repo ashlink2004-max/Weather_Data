73851c85f1b53b9a30331c67f293ccf25f8eed4c
ashlink2004-max <ashlink2004@gmail.com>
2026-05-14 16:09:49 +0530

    Added API response handling

--- Lambda/weather-dy-to-s3.py
+++ Lambda/weather-dy-to-s3.py
@@ -8,8 +8,10 @@ table = dynamodb.Table('weather-data-1')
 
 API_KEY = "your_api_key"
 def lambda_handler(event, context):
-    
+
 city = "Kochi"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"    
 
+response = urllib.request.urlopen(url)
+data = json.loads(response.read())
